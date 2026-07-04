f4ad48d122e4890de392f278edd75730c9f8ac23
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-07-04 22:26:27 +0530

    3978-unique-middle-element Easy .cpp UNKNOWN
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Easy/3978-unique-middle-element/3978-unique-middle-element.cpp
+++ C++/Easy/3978-unique-middle-element/3978-unique-middle-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isMiddleElementUnique(vector<int>& nums) {
-        map<int, int> mp;
+        unordered_map<int, int> mp;
         for(auto it : nums){
             mp[it]++;
         }
