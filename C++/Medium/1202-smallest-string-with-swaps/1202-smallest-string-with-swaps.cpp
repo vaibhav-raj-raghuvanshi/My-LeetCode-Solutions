42ca8644875b4e623e593549411e7fe3432a1673
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-07-08 04:03:10 +0530

    1202-smallest-string-with-swaps Medium .cpp Array
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

---
+++ C++/Medium/1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -0,0 +1,59 @@
+class DSU{
+    vector<int> parent;
+public:
+    DSU(int n){
+        for(int i = 0;i<n;i++){
+            parent.push_back(i);
+        }
+    }
+
+    int find(int x){
+        if(parent[x] == x){
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    void merge(int x, int y){
+        int parX = find(x);
+        int parY = find(y);
+
+        if(parX == parY){
+            return;
+        }
+        if(parX > parY){
+            swap(parX, parY);
+        }
+
+        parent[parY] = parX;
+    }
+};
+
+class Solution {
+public:
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        int n = s.size();
+        DSU dsu(n);
+        for(auto it : pairs){
+            dsu.merge(it[0], it[1]);
+        }
+        for(int i = 0;i<n;i++){
+            // cout << dsu.find(i) << " ";
+        }
+        for(int i = 0;i<n;i++){
+            dsu.find(n - i - 1);
+        }
+        unordered_map<int,priority_queue<char, vector<char>, greater<>>> mp;
+        for(int i = 0;i<n;i++){
+            mp[dsu.find(i)].push(s[i]);
+        }
+        string sol = "";
+        for(int i = 0;i<n;i++){
+            // cout << mp[dsu.find(i)].top() << " ";
+            sol.push_back(mp[dsu.find(i)].top());
+            mp[dsu.find(i)].pop();
+
+        }
+        return sol;
+    }
+};
