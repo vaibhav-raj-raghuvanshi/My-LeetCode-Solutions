dda81ac21b3fbcbb76dd788671cbf3f5f70ad1c5
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-07-09 06:33:35 +0530

    3532-path-existence-queries-in-a-graph-i Medium .cpp Array
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Medium/3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ C++/Medium/3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,44 +1,20 @@
-class DSU{
-    vector<int> parent;
-public:
-    DSU(int n){
-        for(int i= 0;i<n;i++){
-            parent.push_back(i);
-        }
-    }
-    int find(int x){
-        if(parent[x] == x){
-            return x;
-        }
-        return parent[x] = find(parent[x]);
-    }
-    void merge(int x, int y){
-        int parX = find(x);
-        int parY = find(y);
-        parent[parX] = parY;
-    }
-};
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        priority_queue<pair<int, int>> pq;
-        for(int i = 0;i<n;i++){
-            pq.push({nums[i], i});
-        }
-        DSU dsu(n);
-        pair<int, int> temp = pq.top();
-        while(!pq.empty()){
-            pair<int, int> curr = pq.top();
-            pq.pop();
-            if(abs(temp.first - curr.first) <= maxDiff){
-                dsu.merge(temp.second, curr.second);
+        vector<int> component(n, 0);
+        int compNo = 0;
+        for(int i = 1; i < n; i++){
+            if(nums[i] - nums[i - 1] > maxDiff){
+                compNo++;
             }
-            temp = curr;
+            component[i] = compNo;
         }
         vector<bool> sol;
-        for(auto it : queries){
-            sol.push_back(dsu.find(it[0]) == dsu.find(it[1]));
+
+        for(auto &it : queries){
+            sol.push_back(component[it[0]] == component[it[1]]);
         }
+
         return sol;
     }
 };
