ddd9f504aa066ae9b161bff5832490ead82a07a8
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-07-11 11:50:29 +0530

    2940-find-building-where-alice-and-bob-can-meet Hard .cpp Array
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

---
+++ C++/Hard/2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+
+public:
+    vector<int> leftmostBuildingQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> sol(queries.size(), -1);
+        vector<vector<int>> newQ;
+        for(int i = 0;i<queries.size();i++){
+            auto it = queries[i];
+            int x = min(it[1], it[0]);
+            int y = max(it[1], it[0]);
+            if(x == y || nums[y] > nums[x]){
+                sol[i] = y;
+            }else{
+                newQ.push_back({y, nums[x], i});
+            }
+        }
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq; // value, queryIndex
+        int itr = 0;
+        sort(newQ.begin(), newQ.end());
+        for(int i = 0; i<nums.size();i++){
+            while(!pq.empty() && pq.top()[0] < nums[i]){
+                sol[pq.top()[1]] = i;
+                pq.pop();
+            }
+            while(itr < newQ.size() && newQ[itr][0] == i){
+                pq.push({newQ[itr][1], newQ[itr][2]});
+                itr++;
+            }
+        }
+        return sol;
+    }
+};
