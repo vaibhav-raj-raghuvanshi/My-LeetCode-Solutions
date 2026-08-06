b926702adc96c6e487ec925d21613fd8d623d7dd
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-08-06 16:06:01 +0530

    3348-smallest-divisible-digit-product-ii Hard .cpp Math
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Hard/3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii.cpp
+++ C++/Hard/3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii.cpp
@@ -1,22 +1,25 @@
-
 class Solution {
 private:
     vector<vector<int>> minCount2D;
+
     vector<vector<int>> digitContri = {
-        {0, 0, 0, 0}, 
-        {0, 0, 0, 0}, 
-        {1, 0, 0, 0}, 
-        {0, 1, 0, 0}, 
-        {2, 0, 0, 0}, 
-        {0, 0, 1, 0}, 
-        {1, 1, 0, 0}, 
-        {0, 0, 0, 1}, 
-        {3, 0, 0, 0}, 
-        {0, 2, 0, 0}  
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {1, 0, 0, 0},
+        {0, 1, 0, 0},
+        {2, 0, 0, 0},
+        {0, 0, 1, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 1},
+        {3, 0, 0, 0},
+        {0, 2, 0, 0}
     };
-    void build(int Two, int Three) {
-        minCount2D.assign(Two + 1, vector<int>(Three + 1, INT_MAX / 2));
+
+    void build(int two, int three) {
+
+        minCount2D.assign(two + 1, vector<int>(three + 1, INT_MAX / 2));
         minCount2D[0][0] = 0;
+
         vector<pair<int, int>> digits = {
             {1, 0},
             {0, 1},
@@ -25,136 +28,198 @@ private:
             {3, 0},
             {0, 2}
         };
-        for(int i = 0; i <= Two; i++) {
-            for(int j = 0; j <= Three; j++) {
+
+        for(int i = 0; i <= two; i++) {
+
+            for(int j = 0; j <= three; j++) {
+
                 if(i == 0 && j == 0) {
                     continue;
                 }
+
                 int best = INT_MAX / 2;
+
                 for(auto &it : digits) {
+
                     int prevTwo = max(0, i - it.first);
                     int prevThree = max(0, j - it.second);
+
                     best = min(best, minCount2D[prevTwo][prevThree] + 1);
                 }
+
                 minCount2D[i][j] = best;
             }
         }
     }
-    array<int,4> reduce(array<int,4> Need, int Digit) {
+
+    vector<int> reduce(vector<int> need, int digit) {
+
         for(int i = 0; i < 4; i++) {
-            Need[i] = max(0, Need[i] - digitContri[Digit][i]);
+            need[i] = max(0, need[i] - digitContri[digit][i]);
         }
-        return Need;
+
+        return need;
     }
-    int minCount(array<int,4> Need) {
-        return minCount2D[Need[0]][Need[1]] + Need[2] + Need[3];
+
+    int minCount(vector<int> need) {
+
+        return minCount2D[need[0]][need[1]] + need[2] + need[3];
     }
-    bool canFill(array<int,4> Need, int Length) {
-        return minCount(Need) <= Length;
+
+    bool canFill(vector<int> need, int length) {
+
+        return minCount(need) <= length;
     }
-    string buildSuffix(array<int,4> Need, int Length) {
+
+    string buildSuffix(vector<int> need, int length) {
+
         string sol = "";
-        for(int i = 0; i < Length; i++) {
-            int Remaining = Length - i - 1;
-            for(int Digit = 1; Digit <= 9; Digit++) {
-                auto NewNeed = reduce(Need, Digit);
-                if(canFill(NewNeed, Remaining)) {
-                    sol.push_back(Digit + '0');
-                    Need = NewNeed;
+
+        for(int i = 0; i < length; i++) {
+
+            int remaining = length - i - 1;
+
+            for(int digit = 1; digit <= 9; digit++) {
+
+                vector<int> newNeed = reduce(need, digit);
+
+                if(canFill(newNeed, remaining)) {
+
+                    sol.push_back(digit + '0');
+                    need = newNeed;
                     break;
                 }
             }
         }
+
         return sol;
     }
+
 public:
     string smallestNumber(string num, long long t) {
-        int NeedTwo = 0;
-        int NeedThree = 0;
-        int NeedFive = 0;
-        int NeedSeven = 0;
-        long long Temp = t;
-        while(Temp % 2 == 0) {
-            Temp /= 2;
-            NeedTwo++;
+
+        int needTwo = 0;
+        int needThree = 0;
+        int needFive = 0;
+        int needSeven = 0;
+
+        long long temp = t;
+
+        while(temp % 2 == 0) {
+            temp /= 2;
+            needTwo++;
         }
-        while(Temp % 3 == 0) {
-            Temp /= 3;
-            NeedThree++;
+
+        while(temp % 3 == 0) {
+            temp /= 3;
+            needThree++;
         }
-        while(Temp % 5 == 0) {
-            Temp /= 5;
-            NeedFive++;
+
+        while(temp % 5 == 0) {
+            temp /= 5;
+            needFive++;
         }
-        while(Temp % 7 == 0) {
-            Temp /= 7;
-            NeedSeven++;
+
+        while(temp % 7 == 0) {
+            temp /= 7;
+            needSeven++;
         }
-        if(Temp != 1) {
+
+        if(temp != 1) {
             return "-1";
         }
-        build(NeedTwo, NeedThree);
-        array<int,4> FullNeed = {
-            NeedTwo,
-            NeedThree,
-            NeedFive,
-            NeedSeven
+
+        build(needTwo, needThree);
+
+        vector<int> fullNeed = {
+            needTwo,
+            needThree,
+            needFive,
+            needSeven
         };
+
         int n = num.size();
+
         {
-            array<int,4> Need = FullNeed;
-            bool HasZero = false;
+            vector<int> need = fullNeed;
+
+            bool hasZero = false;
+
             for(char ch : num) {
+
                 if(ch == '0') {
-                    HasZero = true;
+                    hasZero = true;
                     break;
                 }
-                Need = reduce(Need, ch - '0');
+
+                need = reduce(need, ch - '0');
             }
-            if(!HasZero && Need == array<int,4>{0,0,0,0}) {
+
+            if(!hasZero && need == vector<int>{0, 0, 0, 0}) {
                 return num;
             }
         }
-        int FirstZero = n;
+
+        int firstZero = n;
+
         for(int i = 0; i < n; i++) {
+
             if(num[i] == '0') {
-                FirstZero = i;
+                firstZero = i;
                 break;
             }
         }
-        vector<array<int,4>> PrefixNeed(n + 1);
-        PrefixNeed[0] = FullNeed;
+
+        vector<vector<int>> prefixNeed(n + 1);
+
+        prefixNeed[0] = fullNeed;
+
         for(int i = 0; i < n; i++) {
+
             if(num[i] == '0') {
-                PrefixNeed[i + 1] = PrefixNeed[i];
+                prefixNeed[i + 1] = prefixNeed[i];
             }
             else {
-                PrefixNeed[i + 1] = reduce(PrefixNeed[i], num[i] - '0');
+                prefixNeed[i + 1] = reduce(prefixNeed[i], num[i] - '0');
             }
         }
-        int Last = min(n - 1, FirstZero);
-        for(int i = Last; i >= 0; i--) {
-            auto Need = PrefixNeed[i];
-            int StartDigit;
+
+        int last = min(n - 1, firstZero);
+
+        for(int i = last; i >= 0; i--) {
+
+            vector<int> need = prefixNeed[i];
+
+            int startDigit;
+
             if(num[i] == '0') {
-                StartDigit = 1;
+                startDigit = 1;
             }
             else {
-                StartDigit = num[i] - '0' + 1;
+                startDigit = num[i] - '0' + 1;
             }
-            for(int Digit = StartDigit; Digit <= 9; Digit++) {
-                auto NewNeed = reduce(Need, Digit);
-                int Remaining = n - i - 1;
-                if(canFill(NewNeed, Remaining)) {
+
+            for(int digit = startDigit; digit <= 9; digit++) {
+
+                vector<int> newNeed = reduce(need, digit);
+
+                int remaining = n - i - 1;
+
+                if(canFill(newNeed, remaining)) {
+
                     string sol = num.substr(0, i);
-                    sol.push_back(Digit + '0');
-                    sol.append(buildSuffix(NewNeed, Remaining));
+
+                    sol.push_back(digit + '0');
+                    sol.append(buildSuffix(newNeed, remaining));
+
                     return sol;
                 }
             }
         }
-        int MinimumLength = minCount(FullNeed);
-        int FinalLength = max(n + 1, MinimumLength);
-        return buildSuffix(FullNeed, FinalLength);
+
+        int minimumLength = minCount(fullNeed);
+        int finalLength = max(n + 1, minimumLength);
+
+        return buildSuffix(fullNeed, finalLength);
     }
 };
