a1d0f1d21ba7282ef34ef8edbc25a9ef2c38bd6c
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-08-09 18:57:28 +0530

    0241-different-ways-to-add-parentheses Medium .cpp Math
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Medium/0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ C++/Medium/0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,5 +1,7 @@
 class Solution {
 private:
+    int n;
+    vector<vector<vector<int>>> dp;
     int solve(int it1, int it2, char ch){
         if(ch == '+'){
             return it1 + it2;
@@ -10,6 +12,9 @@ private:
         return it1 - it2;
     }
     vector<int> backtrack(int low, int high, string &str){
+        if(dp[low][high].size() > 0){
+            return dp[low][high];
+        }
         vector<int> sol;
         for(int i= low;i <= high;i++){
             vector<int> lr, rr;
@@ -26,10 +31,12 @@ private:
         if(sol.empty()){
             sol.push_back(stoi(str.substr(low, high - low + 1)));
         }
-        return sol;
+        return dp[low][high] = sol;
     }
 public:
     vector<int> diffWaysToCompute(string expression) {
+        n = expression.size();
+        dp = vector<vector<vector<int>>>(n, vector<vector<int>>(n));
         return backtrack(0, expression.size()-1, expression);
     }
 };
