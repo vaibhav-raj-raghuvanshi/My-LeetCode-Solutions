e647060f4bd79b724969287aef612192a66b7e3d
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-08-19 00:49:50 +0530

    0198-house-robber Medium .cpp Array
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Medium/0198-house-robber/0198-house-robber.cpp
+++ C++/Medium/0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1){
+        if(nums.size() == 1){
             return nums[0];
         }
+        int n = nums.size() - 3;
+        int s1, s2;
         nums.push_back(0);
-        for(int i = n-3;i>=0;i--){
-            nums[i]+=max(nums[i+2],nums[i+3]);
+        while(n >= 0){
+            nums[n] += max(nums[n+2], nums[n+3]);
+            n--;
         }
-        return max(nums[0],nums[1]);
+        return max(nums[0], nums[1]);
     }
 };
