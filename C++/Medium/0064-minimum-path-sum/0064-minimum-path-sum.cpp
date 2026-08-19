2536b585f18f877cfc4b80e64ff220a9ae56cc0d
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-08-19 20:47:26 +0530

    0064-minimum-path-sum Medium .cpp Array
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Medium/0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ C++/Medium/0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,14 +2,11 @@ class Solution {
 private:
     vector<vector<int>> dp;
     int backtrack(int i, int j, vector<vector<int>>& grid){
-        if(i == 0 && j == 0){
-            return grid[0][0];
+        if(i == 0 || j == 0){
+            return grid[i][j];
         }
         if(dp[i][j] != -1){
             return dp[i][j];
-        }
-        if(i == 0 || j == 0){
-            return dp[i][j] = grid[i][j] + backtrack(max(i-1, 0), max(j-1, 0), grid); 
         }
         return dp[i][j] = grid[i][j] + min(backtrack(i-1, j, grid), backtrack(i, j-1, grid));
     }
@@ -17,6 +14,12 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         dp = vector<vector<int>> (n, vector<int> (m, -1));
+        for(int i = 1; i<n;i++){
+            grid[i][0] += grid[i-1][0];
+        }
+        for(int i = 1; i<m;i++){
+            grid[0][i] += grid[0][i-1];
+        }
         return backtrack(n-1, m-1, grid);
     }
 };
