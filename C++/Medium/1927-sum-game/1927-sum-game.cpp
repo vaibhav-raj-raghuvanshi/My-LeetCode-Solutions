9cc415de91d37f17539e6a1dd03842b0dcf36359
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-08-23 05:43:03 +0530

    1927-sum-game Medium .cpp Math
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Medium/1927-sum-game/1927-sum-game.cpp
+++ C++/Medium/1927-sum-game/1927-sum-game.cpp
@@ -1,9 +1,27 @@
 class Solution {
 public:
-        bool sumGame(string s) {
-        double res = 0, n = s.length();
-        for (int i = 0; i < n; i++)
-            res += (i < n / 2 ? 1 : -1) * (s[i] == '?' ? 4.5 : s[i] - '0');
-        return res != 0.0;
+    bool sumGame(string s) {
+        double res = 0;
+        double n = s.length();
+        for (int i = 0; i < n; i++) {
+            double sign;
+            if (i < n / 2) {
+                sign = 1;
+            } else {
+                sign = -1;
+            }
+            double value;
+            if (s[i] == '?') {
+                value = 4.5;
+            } else {
+                value = s[i] - '0';
+            }
+            res += sign * value;
+        }
+        if (res != 0.0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
