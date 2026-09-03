203bf41958b4a05cab9a34ba3981b3ec96e9952f
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-09-03 18:56:24 +0530

    0740-delete-and-earn Medium .cpp Array
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

---
+++ C++/Medium/0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        vector<long long int> sol(20001, 0);
+        for(auto it : nums){
+            sol[it] += it;
+        }
+        for(int i = 3;i<20001;i++){
+            sol[i] += max(sol[i-2], sol[i-3]);
+        }
+        return *max_element(sol.begin(), sol.end());
+    }
+};
