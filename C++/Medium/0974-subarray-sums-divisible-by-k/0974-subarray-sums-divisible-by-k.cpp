a7224ce2b07a394251ce6d014c279a1c77b69cc2
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-09-05 13:55:04 +0530

    0974-subarray-sums-divisible-by-k Medium .cpp Array
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Medium/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ C++/Medium/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -4,14 +4,14 @@ public:
         unordered_map<int, int> mp;
         int sol = 0;
         mp[0]++;
-        nums[0] = (nums[0] + k * 10000) % k; 
+        nums[0] = ((nums[0]) % k + k) % k; 
         mp[nums[0]]++;
         if(nums[0] == 0){
             sol++;
         }
         int n = nums.size();
         for(int i = 1;i<n;i++){
-            nums[i] = (nums[i] + k * 10000 + nums[i-1]) % k;
+            nums[i] = (((nums[i] + nums[i-1]) % k) + k) % k;
             sol += mp[nums[i]];
             mp[nums[i]]++;
         }
