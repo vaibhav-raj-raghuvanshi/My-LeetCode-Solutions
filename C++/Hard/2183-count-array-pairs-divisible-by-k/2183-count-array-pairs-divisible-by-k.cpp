3d3fb19f8e7a49d92c43c3c90a35a2a30fe4bbb6
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-09-05 17:57:19 +0530

    2183-count-array-pairs-divisible-by-k Hard .cpp Array
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Hard/2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.cpp
+++ C++/Hard/2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.cpp
@@ -1,50 +1,17 @@
 class Solution {
 private:
-    vector<int> spf;
-    void init(){
-        for(int i = 0;i < 100001;i++){
-            spf.push_back(i);
-        }
-        for(int i = 2; i*i <= 100001;i++){
-            if(spf[i] != i){
-                continue;
-            }
-            int num = i * i;
-            while(num < 100001){
-                spf[num] = i;
-                num += i;
-            }
-        }
-    }
+    // for mutthi
     void add(int k, unordered_map<int, long long>& mp) {
-        vector<pair<int, int>> factors;
-        while (k > 1) {
-            int p = spf[k];
-            int cnt = 0;
-            while (k % p == 0) {
-                k /= p;
-                cnt++;
+        for(int i = 1; i * i <= k; i++){
+            if(k % i == 0){
+                mp[i]++;
+                if(i != k / i)
+                    mp[k / i]++;
             }
-            factors.push_back({p, cnt});
-        }
-        vector<int> divisors = {1};
-        for (auto [p, cnt] : factors) {
-            int sz = divisors.size();
-            int power = 1;
-            for (int e = 1; e <= cnt; e++) {
-                power *= p;
-                for (int i = 0; i < sz; i++) {
-                    divisors.push_back(divisors[i] * power);
-                }
-            }
-        }
-        for (int d : divisors) {
-            mp[d]++;
         }
     }
 public:
     long long countPairs(vector<int>& nums, int k) {
-        init();
         unordered_map<int, long long int> mp;
         long long int sol = 0;
         for(auto it : nums){
