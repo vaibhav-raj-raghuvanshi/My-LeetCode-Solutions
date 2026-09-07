21f928317d2305bac6cb08d4ece454d704983767
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-09-07 14:25:23 +0530

    0940-distinct-subsequences-ii Hard .cpp String
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Hard/0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ C++/Hard/0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int distinctSubseqII(string s) {
         vector<long long int> pre = {1};
-        map<char, long long int> mp;
+        vector<long long int> mp(26, 0);
         long long int mod = 1e9 + 7;
-        mp[s[0]]++;
+        mp[(s[0] - 'a')]++;
         for(int i= 1;i < s.size();i++){
-            pre.push_back((1 + pre[i-1] - mp[s[i]] + mod) % mod);
-            mp[s[i]] += pre[i];
-            mp[s[i]] %= mod;
+            pre.push_back((1 + pre[i-1] - mp[(s[i] - 'a')] + mod) % mod);
+            mp[(s[i] - 'a')] += pre[i];
+            mp[(s[i] - 'a')] %= mod;
             pre[i] += pre[i-1];
             pre[i] %= mod;
         }
