08c23d673d6850bbc63dea89b75d0374a1aa452c
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-09-08 10:50:41 +0530

    3870-count-commas-in-range Easy .cpp Math
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Easy/3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ C++/Easy/3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        long long int t = 1000;
-        long long int ctr = 1, sol = 0;
-        while(t <= n){
-            if(t <= n/1000){
-                sol += ctr * (t*1000 - t);
-            }else{
-                sol += ctr * (n - t + 1);
-            }
-            t *= 1000;
-            ctr++;
-        }
-        return sol;
+        return max(0, n - 999);
     }
 };
