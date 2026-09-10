bdb9bd0f27ca08af8596e421a4fc517c3c8cb84b
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-09-10 11:46:02 +0530

    2265-count-nodes-equal-to-average-of-subtree Medium .cpp Tree
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

---
+++ C++/Medium/2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int sol;
+    pair<int, int> backtrack(TreeNode* root){
+        if(root == NULL){
+            return {0, 0};
+        }
+        pair<int ,int> p1 = backtrack(root->left);
+        pair<int ,int> p2 = backtrack(root->right);
+        if(floor((0.0 + p1.first + p2.first + root -> val)/(p1.second + p2.second + 1)) == root->val){
+            sol++;
+        }
+        return {p1.first+p2.first+root->val, p1.second + p2.second + 1};
+    }
+
+public:
+    int averageOfSubtree(TreeNode* root) {
+        backtrack(root);
+        return sol;
+    }
+};
