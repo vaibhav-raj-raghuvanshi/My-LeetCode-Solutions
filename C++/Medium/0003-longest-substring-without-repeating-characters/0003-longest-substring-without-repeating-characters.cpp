810a6cd78bba61ff19f517a62f4fff61ec22124f
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-09-12 14:53:04 +0530

    0003-longest-substring-without-repeating-characters Medium .cpp Hash Table
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

--- C++/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ C++/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string nums) {
-        int sol = 0;
+        unordered_map<char, int>mp;
+        int sol = min(1, (int)nums.size());
         int low = 0;
-        int high = 0;
-        int n = nums.size();
-        unordered_map<char, int> mp;
-        for(;high < n;high++){
+        mp[nums[0]]++;
+        for(int high = 1;high < nums.size();high++){
             mp[nums[high]]++;
             while(mp[nums[high]] > 1){
                 mp[nums[low]]--;
                 low++;
             }
-            sol = max(sol, high - low + 1);
+            sol = max(high - low + 1, sol);
         }
         return sol;
     }
