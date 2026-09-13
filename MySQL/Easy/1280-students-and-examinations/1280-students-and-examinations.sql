548bdf8bfd09ab52eb650d9f447540fcdd2db624
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-09-13 18:54:40 +0530

    1280-students-and-examinations Easy .sql Database
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

---
+++ MySQL/Easy/1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT st.student_id, st.student_name, sub.subject_name, count(e.subject_name) as attended_exams
+FROM Students st
+JOIN Subjects sub
+LEFT JOIN Examinations e
+ON st.student_id = e.student_id and sub.subject_name = e.subject_name
+GROUP BY st.student_id, sub.subject_name
+ORDER BY st.student_id, sub.subject_name
