d2266bc727e2ffd19275b39dbfb3687fbf9e4402
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-09-13 19:53:33 +0530

    1251-average-selling-price Easy .sql Database
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

---
+++ MySQL/Easy/1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT p.product_id, ROUND(IFNULL(SUM(p.price * u.units) / sum(u.units), 0), 2) as average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id 
+AND u.purchase_date >= p.start_date and
+u.purchase_date <= p.end_date
+GROUP BY p.product_id
