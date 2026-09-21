ad60dabfd94c5140cfab5682a446681249e6a637
Vaibhav Raj Singh <167506449+vaibhav-raj-raghuvanshi@users.noreply.github.com>
2026-09-22 05:08:12 +0530

    0528-random-pick-with-weight Medium .cpp Array
    Solved by: vaibhav_jpd 
    LinkedIn: https://www.linkedin.com/in/vaibhav-raj-singh-/
    
    LeetCode: leetcode.com/u/rosvert/
    
    Portfolio:
    https://www.vaibhavrajsingh.in/
    
    #vaibhav-raj-singh
    #jiit
    #Jaypee
    
    Student @Jaypee Institute of Information Technology

---
+++ C++/Medium/0528-random-pick-with-weight/0528-random-pick-with-weight.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    vector<long long int> nums;
+public:
+    Solution(vector<int>& w) {
+        long long int prev = 0;
+        for(auto it : w){
+            prev += it;
+            nums.push_back(prev);
+        }
+    }
+    
+    int pickIndex() {
+        int r = rand()%nums[nums.size()-1];
+        return upper_bound(nums.begin(), nums.end(), r) - nums.begin();
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
